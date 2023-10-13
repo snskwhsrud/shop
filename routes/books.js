e02a0f73bf492f488cc49f817db4fd0d725a9837
snskwhsrud <snskwhsrud@gmail.com>
2023-10-13 18:22:02 +0900

    10-13일 수업 끝

--- routes/books.js
+++ routes/books.js
@@ -19,6 +19,7 @@ router.post("/search/insert", function (req, res) {
   //console.log(title,authors,price,publisher,image,contents);
   const sql1 = "select * from books where isbn=?";
   db.get().query(sql1, [isbn], function (err, rows) {
+    if (err) console.log("isbn검색:", err);
     if (rows.length > 0) {
       //이미도서가 등록된 경우
       res.send("1");
@@ -35,6 +36,30 @@ router.post("/search/insert", function (req, res) {
         }
       );
     }
+  });
+});
+
+//도서목록 JSON
+router.get("/list.json", function (req, res) {
+  const page = req.query.page;
+  const start = (parseInt(page) - 1) * 5;
+  const sql = "select * from books order by bid desc limit ?, 5";
+  db.get().query(sql, [start], function (err, rows) {
+    if (err) console.log("도서목록JSON:", err);
+    res.send(rows);
+  });
+});
+
+//도서목록 페이지 이동
+router.get("/list", function (req, res) {
+  res.render("index", { title: "도서목록", pageName: "books/list.ejs" });
+});
+
+//데이터갯수
+router.get("/count", function (req, res) {
+  const sql = "select count(*) total from books";
+  db.get().query(sql, function (err, rows) {
+    res.send(rows[0]);
   });
 });
 
